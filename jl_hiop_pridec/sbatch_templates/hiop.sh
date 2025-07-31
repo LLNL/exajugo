65cc4d372ddc34c655cd50a095dde8678697ae5e
Claudio Santiago <santiago10@llnl.gov>
2025-07-31 13:43:52 -0700

    added file to check status and instruction for tsslope

--- jl_hiop_pridec/sbatch_templates/hiop.sh
+++ jl_hiop_pridec/sbatch_templates/hiop.sh
@@ -18,6 +18,7 @@ mkdir -p "${OUTPUT_DIR}"
   echo "PATH_TO_HSLLIB=${PATH_TO_HSLLIB}"
   echo "HIOP_INSTALL_DIR=${HIOP_INSTALL_DIR}"
   echo "PATH_TO_TSSLOPE=${PATH_TO_TSSLOPE}"
+  echo "NORMALIZE_X=${NORMALIZE_X}"
   echo "SLURM_JOB_ID=${SLURM_JOB_ID}"
 } > "${OUTPUT_DIR}/environment_vars.txt"
 
